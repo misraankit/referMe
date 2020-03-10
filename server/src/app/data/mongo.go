fc98035a1645a73d717c415272d968d4a87bef4b
Ankit <ankmisra@deloitte.com>
2020-03-10 16:28:09 +0530

    Added Mongo Connection

--- server/src/app/data/mongo.go
+++ server/src/app/data/mongo.go
@@ -1,6 +1,9 @@
 package mongo
+
 import (
-  "context"
-  "github.com/mongodb/mongo-go-driver/mongo"
+	"context"
+
+	"github.com/mongodb/mongo-go-driver/mongo"
 )
-var Client, err = mongo.Connect(context.Background(), "Insert your MongoDB URI here!", nil)v
+
+var Client, err = mongo.Connect(context.Background(), "mongodb+srv://nithyavardhan:hoaxapp@69@cluster0-xwnvc.mongodb.net/test?retryWrites=true&w=majority", nil)
